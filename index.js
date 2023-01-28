82618d42fb4624f1f1acc5dcad03efdbea2c55fc
Philip Demand <philipdemand@yahoo.com>
2023-01-28 15:37:25 -0500

    commit tree 268d9c093ec88e312de16348587500146bd26d4b

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    const newString = string.substring(1)
+    const newerString = (string[0].toUpperCase()) + newString
+    return newerString
+  }
+  static sanitize(string) {
+    const evenNewerString = string.replace(/[^a-zA-Z0-9-'\s]/g, "");
+    return evenNewerString
+  }
+  static titleize(string) {
+    let upper = string.replace(/(\b[a-z])/g, c => c.toUpperCase());
+    let andDown = upper.replace(
+      /((The|And|From|But|An|Of|A|For|At|By)((?=,)|(?= )))/g,
+      c => c.charAt(0).toLowerCase() + c.substring(1)
+    );
+    let subAndDown = andDown.substring(1)
+    return andDown[0][0].toUpperCase() + subAndDown
+  } 
 }
